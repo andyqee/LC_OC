7263eef7618205c6482422c9b9c3b7d640aa300e
andyqee <vitasone@gmail.com>
2016-10-30 16:55:41 +0800

    Initial Commit

---
+++ LCOBJC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LCOBJC
+//
+//  Created by andy on 30/10/2016.
+//  Copyright © 2016 andy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
